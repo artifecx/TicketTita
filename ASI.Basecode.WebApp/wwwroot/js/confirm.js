3adf6b68a402ac714f576efd6f84e9530a39455c
artifecx <kebeniga@gmail.com>
2024-07-18 09:42:12 +0800

    Add confirmation to ticket actions that cannot be undone (mark as resolved, submit feedback)

---
+++ ASI.Basecode.WebApp/wwwroot/js/confirm.js
@@ -0,0 +1,21 @@
+﻿var confirmationCallback;
+var originalModalId;
+function displayConfirmationModal(callback, message, originalModal) {
+    confirmationCallback = callback;
+    originalModalId = originalModal;
+    document.getElementById('confirmationModalMessage').textContent = `${message}. Are you sure you want to proceed?`;
+    $('#confirmationModal').modal('show');
+}
+
+$('#confirmationModal .btn-primary').on('click', function () {
+    if (typeof confirmationCallback === 'function') {
+        confirmationCallback();
+    }
+});
+
+$('#confirmationModal').on('hidden.bs.modal', function () {
+    $('#confirmationModal').modal('hide');
+    $('#'+originalModalId).modal('show');
+    confirmationCallback = null;
+    originalModalId = null;
+});
